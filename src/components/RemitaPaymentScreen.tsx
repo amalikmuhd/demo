f254a1af5ec8da243f599c6a6f06d2e4787e3a83
Abdulmalik Muhammad <amalikmuhdd@gmail.com>
2024-07-19 11:16:19 +0100

    update

--- src/components/RemitaPaymentScreen.tsx
+++ src/components/RemitaPaymentScreen.tsx
@@ -9,7 +9,7 @@ interface RemitaFakeProps {
 const RemitaPaymentScreen = ({ handleNext }: RemitaFakeProps) => {
   return (
     <div className="fixed top-0 left-0 z-50 bg-white">
-      <img src={remita} className="w-[100%] h-full " />
+      <img src={remita} className="w-[100%] h-[60%] " />
       <div className="w-[100%] flex flex-row justify-center">
         <div className="w-[60%] flex justify-center items-center gap-2 mt-2">
           <CustomButton
