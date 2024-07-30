d8003161b1cd80d6afb0e6f372025e05c21ccb73
Abdulmalik Muhammad <amalikmuhdd@gmail.com>
2024-07-31 00:48:36 +0100

    update

--- src/components/FillForm.tsx
+++ src/components/FillForm.tsx
@@ -9,22 +9,18 @@ import {
   districtData,
   educationData,
   genderData,
-  // educationData,
-  // genderData,
   landuseData,
   maritalStatusData,
-  // maritalStatusData,
   purposeData,
   religionData,
-  // religionData,
   titleData,
 } from "../data";
 import { IFillForm } from "../types";
 import CustomAreaText from "./CustomAreaText";
 import CustomFileInput from "./CustomFileInput";
 import { useLocation } from "react-router-dom";
-// import { useLocation } from "react-router-dom";
-// import { useQuery } from "@tanstack/react-query";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface FillFormForm {
   handleNext: () => void | undefined;
@@ -34,6 +30,57 @@ interface FillFormForm {
   phoneNumber?: string;
   middle?: string;
 }
+
+// Validation schema
+const validationSchema = Yup.object().shape({
+  title: Yup.string().required("Title is required"),
+  office: Yup.string().required("Office is required"),
+  orgemail: Yup.string().required("Email is required"),
+  officePhone: Yup.string().required("Office Phone is required"),
+  mobile: Yup.string().required("Mobile is required"),
+  lc: Yup.string().required("Local Government is required"),
+  nor: Yup.string().required("Nation of Registration is required"),
+  rc: Yup.string().required("Registration Number (RC) is required"),
+  designation: Yup.string().required("Designation is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  phoneNumber: Yup.string()
+    .required("Phone No. is required")
+    .matches(/^[0-9]{10,11}$/, "Phone number must be 10 or 11 digits"),
+  gender: Yup.string().required("Gender is required"),
+  dob: Yup.date().required("Date of Birth is required").nullable(),
+  occupation: Yup.string().required("Occupation is required"),
+  children: Yup.number()
+    .required("Number of Children is required")
+    .positive("Must be positive")
+    .integer("Must be an integer"),
+  nationality: Yup.string().required("Nationality is required"),
+  origin: Yup.string().required("State of Origin is required"),
+  localGov: Yup.string().required("Local Gov. is required"),
+  maritalStatus: Yup.string().required("Marital Status is required"),
+  religion: Yup.string().required("Religion is required"),
+  education: Yup.string().required("Education is required"),
+  houseNo: Yup.string().required("House No is required"),
+  streetName: Yup.string().required("Street Name is required"),
+  streetExtension: Yup.string(),
+  cityDistrict: Yup.string().required("City-District is required"),
+  state: Yup.string().required("State is required"),
+  country: Yup.string().required("Country is required"),
+  popmb: Yup.string().required("P.O/P.M.B is required"),
+  co: Yup.string().required("C/O is required"),
+  additional: Yup.string(),
+  homePhone: Yup.string().matches(/^[0-9]{10,11}$/, "Phone number must be 10 or 11 digits"),
+  nin: Yup.string().required("NIN is required").length(11, "NIN must be 11 digits"),
+  homePhoneeeee: Yup.string(),
+  landuse: Yup.string().required("Landuse is required"),
+  purpose: Yup.string().required("Purpose is required"),
+  council: Yup.string().required("Council is required"),
+  district: Yup.string().required("District is required"),
+  plotSize: Yup.number().required("Plot Size is required").positive("Must be positive"),
+  comment: Yup.string(),
+  // signature: Yup.mixed().required('Signature is required').test('fileType', 'Unsupported Format', (value) => {
+  //   return value && value[0] as never && ['image/jpeg', 'image/png'].includes(value[0].type);
+  // }),
+});
 
 const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, email, phoneNumber }) => {
   const params = useLocation();
@@ -46,14 +93,14 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
       phoneNumber: phoneNumber,
       middle: "",
     },
-    // resolver: yupResolver(makePaymentFormSchema),
+    resolver: yupResolver(validationSchema) as never,
   });
 
   const onSubmit: SubmitHandler<IFillForm> = () => {
     handleNext();
   };
 
-  return params.state.type === "Individual" ? (
+  return params.state.type !== "Individual" ? (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
       <div className="w-[60%]">
         <div className="flex flex-row items-start gap-1  mt-[16px] mb-[16px]">
@@ -164,33 +211,6 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           <div className={`bg-gray-400 h-[1px] w-4/4`} />
           <div className="mb-[40px]" />
         </>
-        {/* <>
-          <div className="flex flex-row items-start gap-1  mt-[16px]">
-            <label className="text-left mb-[30px] font-inter font-medium text-[14px]">Contact Person</label>
-          </div>
-          <div className="flex flex-row justify-between gap-6">
-            <CustomInput name="designation" label={"Designation"} control={control as never} />
-            <CustomSelect
-              control={control as never}
-              name="gender"
-              label="Gender"
-              asterisk
-              placeholder=" "
-              options={genderData}
-            />
-            
-          </div>
-
-          <div className="flex flex-row justify-between gap-6 mt-[16px]">
-            <CustomInput name="occupation" label={"Occupation"} control={control as never} />
-            <CustomInput name="children" label={"Children"} control={control as never} />
-          </div>
-          <div className="mt-[40px]" />
-          <div className={`bg-gray-400 h-[1px] w-4/4`} />
-          <div className="mb-[40px]" />
-        </> */}
-
-        {/* step 4 */}
         <>
           <div className="flex flex-row justify-between gap-6">
             <CustomSelect
@@ -226,7 +246,6 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           <div className={`bg-gray-400 h-[1px] w-4/4`} />
           <div className="mb-[40px]" />
         </>
-        {/* step 5 */}
 
         <>
           <div className="flex flex-row items-start gap-1  mt-[16px]">
@@ -259,21 +278,6 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           <div className="mb-[40px]" />
         </>
 
-        {/* <>
-          <div className="flex flex-row justify-between gap-6">
-            <CustomInput name="officePhone" label={"Office Phone:"} asterisk control={control as never} />
-            <CustomInput name="mobile" label={"Mobile"} asterisk control={control as never} />
-          </div>
-          <div className="flex flex-row justify-between gap-6 mt-[16px]">
-            <CustomInput name="facsimile" label={"Facsimile"} control={control as never} />
-            <CustomInput name="emailss" label={"Email"} asterisk control={control as never} />
-          </div>
-          <div className="mt-[40px]" />
-          <div className={`bg-gray-400 h-[1px] w-4/4`} />
-          <div className="mb-[40px]" />
-        </> */}
-
-        {/*  step 6 */}
         <>
           <div className="flex flex-row justify-between gap-6">
             <CustomInput name="homePhone" label={"Home Phone::"} control={control as never} />
@@ -404,25 +408,9 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           </div>
 
           <div className="mt-[40px]" />
-          {/* <div className={`bg-gray-400 h-[1px] w-4/4`} /> */}
           <div className="mb-[40px]" />
         </>
-        {/* step 13 */}
-        {/* <>
-          <div className="flex flex-row justify-between gap-6 mt-[16px] mb-[40px]">
-            <div className="flex flex-row items-start gap-1 mb-[16px]">
-              <label className="text-left mb-[8px] font-inter font-normal text-sm text-black/40">
-                <label className="text-left font-inter font-normal text-sm text-[#DD1D1D]">Note: </label>
-                It is a punishable offence to provide any false information and or make any false statements or claim
-                when completing this form. Where it is subsequently discovered that a Certificate of Occupancy was
-                issued based on false or inaccurate information, the Minister may in his sole discretion, revoke such
-                Certificate of Occupancy. The minister reserves the right to reject any application for not properly or
-                fully completed and shall not incur any liability for any such revocation". The information you supply
-                on this form is public knowledge and may be published in the media.
-              </label>
-            </div>
-          </div>
-        </> */}
+
         <CustomButton name="Submit form" trailingIcon={<FaArrowRight />} />
         <div className="mb-[40px]" />
       </div>
@@ -475,10 +463,10 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           </div>
           <div className="flex flex-row justify-between gap-6 mt-[16px]">
             <CustomInput name="rc" label={"Registration Number (RC)"} asterisk control={control as never} />
-            <CustomInput name="rccccc" label={"Nation of Registration"} asterisk control={control as never} />
+            <CustomInput name="nor" label={"Nation of Registration"} asterisk control={control as never} />
           </div>
           <div className="flex flex-row justify-between gap-6 mt-[16px]">
-            <CustomInput name="rcc" label={"Local Government"} asterisk control={control as never} />
+            <CustomInput name="lc" label={"Local Government"} asterisk control={control as never} />
 
             <div className="flex-1" />
           </div>
@@ -508,83 +496,6 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           <div className={`bg-gray-400 h-[1px] w-4/4`} />
           <div className="mb-[40px]" />
         </>
-        {/* <>
-          <div className="flex flex-row items-start gap-1  mt-[16px]">
-            <label className="text-left mb-[30px] font-inter font-medium text-[14px]">Contact Person</label>
-          </div>
-          <div className="flex flex-row justify-between gap-6">
-            <CustomInput name="designation" label={"Designation"} control={control as never} />
-            <CustomSelect
-              control={control as never}
-              name="gender"
-              label="Gender"
-              asterisk
-              placeholder=" "
-              options={genderData}
-            />
-            
-          </div>
-
-          <div className="flex flex-row justify-between gap-6 mt-[16px]">
-            <CustomInput name="occupation" label={"Occupation"} control={control as never} />
-            <CustomInput name="children" label={"Children"} control={control as never} />
-          </div>
-          <div className="mt-[40px]" />
-          <div className={`bg-gray-400 h-[1px] w-4/4`} />
-          <div className="mb-[40px]" />
-        </> */}
-        {/* step 3 */}
-        {/* <>
-          <div className="flex flex-row justify-between gap-6">
-            <CustomInput name="nationality" label={"Nationality"} asterisk control={control as never} />
-            <CustomInput name="origin" label={"State of Origin"} asterisk control={control as never} />
-          </div>
-
-          <div className="flex flex-row justify-between gap-6 mt-[16px]">
-            <CustomInput name="localGov" label={"Local Gov."} control={control as never} />
-            <div className="flex-1" />
-          </div>
-          <div className="mt-[40px]" />
-          <div className={`bg-gray-400 h-[1px] w-4/4`} />
-          <div className="mb-[40px]" />
-        </> */}
-        {/* step 4 */}
-        {/* <>
-          <div className="flex flex-row justify-between gap-6">
-            <CustomSelect
-              control={control as never}
-              name="maritalStatus"
-              label="Marital Status"
-              asterisk
-              placeholder=" "
-              options={maritalStatusData}
-            />
-            <CustomSelect
-              control={control as never}
-              name="religion"
-              label="Religion"
-              asterisk
-              placeholder=" "
-              options={religionData}
-            />
-          </div>
-
-          <div className="flex flex-row justify-between gap-6 mt-[16px]">
-            <CustomSelect
-              control={control as never}
-              name="education"
-              label="Education"
-              asterisk
-              placeholder=" "
-              options={educationData}
-            />
-            <div className="flex-1" />
-          </div>
-          <div className="mt-[40px]" />
-          <div className={`bg-gray-400 h-[1px] w-4/4`} />
-          <div className="mb-[40px]" />
-        </> */}
-        {/* step 5 */}
 
         <>
           <div className="flex flex-row items-start gap-1  mt-[16px]">
@@ -636,22 +547,6 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           <div className="mb-[40px]" />
         </>
 
-        {/*  step 6 */}
-        {/* <>
-          <div className="flex flex-row justify-between gap-6">
-            <CustomInput name="homePhone" label={"Home Phone::"} control={control as never} />
-            <CustomInput name="office" label={"Office:"} control={control as never} />
-          </div>
-          <div className="flex flex-row justify-between gap-6 mt-[16px]">
-            <CustomInput name="emails" label={"Email"} asterisk control={control as never} />
-            <div className="flex-1" />
-          </div>
-
-          <div className="mt-[40px]" />
-          <div className={`bg-gray-400 h-[1px] w-4/4`} />
-          <div className="mb-[40px]" />
-        </> */}
-        {/*  step 7 */}
         <>
           <div className="flex flex-row justify-between gap-6 mt-[16px]">
             <label className="text-left mb-[8px] font-inter font-normal text-sm">
@@ -767,25 +662,9 @@ const FillForm: React.FC<FillFormForm> = ({ handleNext, firstName, lastName, ema
           </div>
 
           <div className="mt-[40px]" />
-          {/* <div className={`bg-gray-400 h-[1px] w-4/4`} /> */}
           <div className="mb-[40px]" />
         </>
-        {/* step 13 */}
-        {/* <>
-          <div className="flex flex-row justify-between gap-6 mt-[16px] mb-[40px]">
-            <div className="flex flex-row items-start gap-1 mb-[16px]">
-              <label className="text-left mb-[8px] font-inter font-normal text-sm text-black/40">
-                <label className="text-left font-inter font-normal text-sm text-[#DD1D1D]">Note: </label>
-                It is a punishable offence to provide any false information and or make any false statements or claim
-                when completing this form. Where it is subsequently discovered that a Certificate of Occupancy was
-                issued based on false or inaccurate information, the Minister may in his sole discretion, revoke such
-                Certificate of Occupancy. The minister reserves the right to reject any application for not properly or
-                fully completed and shall not incur any liability for any such revocation". The information you supply
-                on this form is public knowledge and may be published in the media.
-              </label>
-            </div>
-          </div>
-        </> */}
+
         <CustomButton name="Submit form" trailingIcon={<FaArrowRight />} />
         <div className="mb-[40px]" />
       </div>
