9ed69c45a442b8a9dbae7f2f324711fe2bcad062
Abdulmalik Muhammad <amalikmuhdd@gmail.com>
2024-08-06 09:54:18 +0100

    update api service

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
+  // baseURL: "https://fapay-backend.onrender.com/api/v1/",
   baseURL: "https://fapay-backend.onrender.com/api/v1/",
 });
 
